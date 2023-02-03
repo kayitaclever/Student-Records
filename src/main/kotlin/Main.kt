004fd70c5d45b5ca56a2217cc69f3d96538e87a0
kayitaclever <kayitaramirwaaclever@gmail.com>
2023-02-03 08:14:51 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+
+    var Group = " student record "
+    println(Group)
+    var fullname = "clever"
+    println(fullname)
+    var Age: Byte = 20
+    println(Byte)
+    var phoneN: Int = 25085943
+    println(phoneN)
+
+    var Weight: Byte = 56
+    println(Weight)
+    var Citizen: Boolean = false
+println(Citizen)}
+
+
+
+
